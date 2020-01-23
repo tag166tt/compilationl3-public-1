c5d45bf8cbc872f8ec46d7d8f986b9d4b50942a5
Nicolas Bourras <vivescere@protonmail.com>
2020-01-23 11:21:03 +0100

    Initial import, intellij module creation, and code re-format

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,39 +1,33 @@
-import sc.parser.*;
-import sc.lexer.*;
-import sc.node.*;
-import java.io.*;
-//import sa.*;
-//import ts.*;
-//import c3a.*;
-//import nasm.*;
-//import fg.*;
+import org.sablecc.sablecc.lexer.Lexer;
+import org.sablecc.sablecc.node.Start;
+import org.sablecc.sablecc.parser.Parser;
 
-public class Compiler
-{
-    public static void main(String[] args)
-    {
-	PushbackReader br = null;
-	String baseName = null;
-	try {
-	    if (0 < args.length) {
-		br = new PushbackReader(new FileReader(args[0]));
-		baseName = removeSuffix(args[0], ".l");
-	    }
-	    else{
-		System.out.println("il manque un argument");
-	    }
-	}
-	catch (IOException e) {
-	    e.printStackTrace();
-	} 
-	try {
-	    // Create a Parser instance.
-	    Parser p = new Parser(new Lexer(br));
-	    // Parse the input.
-	    Start tree = p.parse();
-	    
-	    System.out.println("[SC]");
-	    tree.apply(new Sc2Xml(baseName));
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+public class Compiler {
+    public static void main(String[] args) {
+        PushbackReader br = null;
+        String baseName = null;
+        try {
+            if (0 < args.length) {
+                br = new PushbackReader(new FileReader(args[0]));
+                baseName = removeSuffix(args[0], ".l");
+            } else {
+                System.out.println("il manque un argument");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            // Create a Parser instance.
+            Parser p = new Parser(new Lexer(br));
+            // Parse the input.
+            Start tree = p.parse();
+
+            System.out.println("[SC]");
+            tree.apply(new Sc2Xml(baseName));
 
 	    /*System.out.println("[SA]");
 	    Sc2sa sc2sa = new Sc2sa();
@@ -60,22 +54,19 @@ public class Compiler
 	    System.out.println("[FLOW GRAPH SOLVE]");
 	    FgSolution fgSolution = new FgSolution(nasm, fg);
 	    fgSolution.affiche(baseName);*/
-	    
 
-	    
-	}
-	catch(Exception e){
-	    System.out.println(e.getMessage());
-	}
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
-    public static String removeSuffix(final String s, final String suffix)
-    {
-	if (s != null && suffix != null && s.endsWith(suffix)){
-	    return s.substring(0, s.length() - suffix.length());
-	}
-	return s;
+    public static String removeSuffix(final String s, final String suffix) {
+        if (s != null && suffix != null && s.endsWith(suffix)) {
+            return s.substring(0, s.length() - suffix.length());
+        }
+        return s;
     }
-    
+
 }
