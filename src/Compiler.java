8a68733f87999bb47914add68726bb5918f8cf93
Nicolas Bourras <vivescere@gmail.com>
2020-01-24 11:41:57 +0100

    Fixed indentation

--- src/Compiler.java
+++ src/Compiler.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.io.PushbackReader;
 
 public class Compiler {
+
     public static void main(String[] args) {
         PushbackReader br = null;
         String baseName = null;
+
         try {
             if (0 < args.length) {
                 br = new PushbackReader(new FileReader(args[0]));
@@ -20,6 +22,7 @@ public class Compiler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         try {
             // Create a Parser instance.
             Parser p = new Parser(new Lexer(br));
@@ -29,38 +32,35 @@ public class Compiler {
             System.out.println("[SC]");
             tree.apply(new Sc2Xml(baseName));
 
-	    /*System.out.println("[SA]");
-	    Sc2sa sc2sa = new Sc2sa();
-	    tree.apply(sc2sa);
-	    SaNode saRoot = sc2sa.getRoot();
-	    new Sa2Xml(saRoot, baseName);
-		    
-	    System.out.println("[TABLE SYMBOLES]");
-	    Ts table = new Sa2ts(saRoot).getTableGlobale();
-	    table.afficheTout(baseName);
+    	    /*System.out.println("[SA]");
+            Sc2sa sc2sa = new Sc2sa();
+            tree.apply(sc2sa);
+            SaNode saRoot = sc2sa.getRoot();
+            new Sa2Xml(saRoot, baseName);
 
-	    System.out.println("[C3A]");
-	    C3a c3a = new Sa2c3a(saRoot, table).getC3a();
-	    c3a.affiche(baseName);
+            System.out.println("[TABLE SYMBOLES]");
+            Ts table = new Sa2ts(saRoot).getTableGlobale();
+            table.afficheTout(baseName);
 
-	    System.out.println("[NASM]");
-	    Nasm nasm = new C3a2nasm(c3a, table).getNasm();
-	    nasm.affiche(baseName);
+            System.out.println("[C3A]");
+            C3a c3a = new Sa2c3a(saRoot, table).getC3a();
+            c3a.affiche(baseName);
 
-	    System.out.println("[FLOW GRAPH]");
-	    Fg fg = new Fg(nasm);
-	    fg.affiche(baseName);
+            System.out.println("[NASM]");
+            Nasm nasm = new C3a2nasm(c3a, table).getNasm();
+            nasm.affiche(baseName);
 
-	    System.out.println("[FLOW GRAPH SOLVE]");
-	    FgSolution fgSolution = new FgSolution(nasm, fg);
-	    fgSolution.affiche(baseName);*/
+            System.out.println("[FLOW GRAPH]");
+            Fg fg = new Fg(nasm);
+            fg.affiche(baseName);
 
-
+            System.out.println("[FLOW GRAPH SOLVE]");
+            FgSolution fgSolution = new FgSolution(nasm, fg);
+            fgSolution.affiche(baseName);*/
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
 
     public static String removeSuffix(final String s, final String suffix) {
         if (s != null && suffix != null && s.endsWith(suffix)) {
