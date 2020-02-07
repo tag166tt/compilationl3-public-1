76f9519ee5084f7db13390e1a0ef77956453ca60
Nicolas Bourras <vivescere@gmail.com>
2020-02-07 07:35:56 +0100

    Updated linux bash script to remove temporary files before building

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rm -rf sc
+find ../test/input -name '*.sa' -delete
+find ../test/input -name '*.sc' -delete
+find ../test/input -name '*.ts' -delete
+find ../test/input -name '*.xml' -delete
 java -jar ../sablecc.jar grammaireL.sablecc
