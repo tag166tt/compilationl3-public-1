246f86585d091eab51a892436d0d58d37dea7df8
Yashovardhan Thevalil <yashthevalil@gmail.com>
2020-02-18 13:46:28 +0100

    I CAN HAZ PYTHON, I CAN HAZ INDENTS

--- src/build.bat
+++ src/build.bat
@@ -4,4 +4,5 @@ del ..\test\input\*.sa /s
 del ..\test\input\*.sc /s
 del ..\test\input\*.ts /s
 del ..\test\input\*.xml /s
+del ..\test\input\*.c3a /s
 java -jar ../sablecc.jar grammaireL.sablecc
--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,5 @@ find ../test/input -name '*.sa' -delete
 find ../test/input -name '*.sc' -delete
 find ../test/input -name '*.ts' -delete
 find ../test/input -name '*.xml' -delete
+find ../test/input -name '*.c3a' -delete
 java -jar ../sablecc.jar grammaireL.sablecc
