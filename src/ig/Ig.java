7ac0190e7c9136b10d377988f0a9a8cacae0815d
Nicolas Bourras <vivescere@gmail.com>
2020-04-08 09:52:09 +0200

    eppic fail Tony
    Reformatted Ig and ColorGraph, stubbed out methods with a
    RuntimeException so that the project compiles.

--- src/ig/Ig.java
+++ src/ig/Ig.java
@@ -1,11 +1,13 @@
 package ig;
 
-import fg.*;
-import nasm.*;
-import util.graph.*;
-import util.intset.*;
-import java.util.*;
-import java.io.*;
+import fg.FgSolution;
+import nasm.Nasm;
+import util.graph.Graph;
+import util.graph.Node;
+import util.graph.NodeList;
+
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class Ig {
     public Graph graph;
@@ -14,53 +16,53 @@ public class Ig {
     public Nasm nasm;
     public Node int2Node[];
 
-    
-    public Ig(FgSolution fgs){
-	this.fgs = fgs;
- 	this.graph = new Graph();
-	this.nasm = fgs.nasm;
-	this.regNb = this.nasm.getTempCounter();
-	this.int2Node = new Node[regNb];
-	this.build();
+
+    public Ig(FgSolution fgs) {
+        this.fgs = fgs;
+        this.graph = new Graph();
+        this.nasm = fgs.nasm;
+        this.regNb = this.nasm.getTempCounter();
+        this.int2Node = new Node[regNb];
+        this.construction();
     }
 
-    public void construction(){
+    public void construction() {
+        throw new RuntimeException("Not implemented yet");
     }
 
-    public int[] getPrecoloredTemporaries()
-    {
-    }
-
-
-    public void allocateRegisters(){
+    public int[] getPrecoloredTemporaries() {
+        throw new RuntimeException("Not implemented yet");
     }
 
 
-    public void affiche(String baseFileName){
-	String fileName;
-	PrintStream out = System.out;
-	
-	if (baseFileName != null){
-	    try {
-		baseFileName = baseFileName;
-		fileName = baseFileName + ".ig";
-		out = new PrintStream(fileName);
-	    }
-	    
-	    catch (IOException e) {
-		System.err.println("Error: " + e.getMessage());
-	    }
-	}
-	
-	for(int i = 0; i < regNb; i++){
-	    Node n = this.int2Node[i];
-	    out.print(n + " : ( ");
-	    for(NodeList q=n.succ(); q!=null; q=q.tail) {
-		out.print(q.head.toString());
-		out.print(" ");
-	    }
-	    out.println(")");
-	}
+    public void allocateRegisters() {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+
+    public void affiche(String baseFileName) {
+        String fileName;
+        PrintStream out = System.out;
+
+        if (baseFileName != null) {
+            try {
+                baseFileName = baseFileName;
+                fileName = baseFileName + ".ig";
+                out = new PrintStream(fileName);
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+
+        for (int i = 0; i < regNb; i++) {
+            Node n = this.int2Node[i];
+            out.print(n + " : ( ");
+            for (NodeList q = n.succ(); q != null; q = q.tail) {
+                out.print(q.head.toString());
+                out.print(" ");
+            }
+            out.println(")");
+        }
     }
 }
 	    
--- src/util/graph/ColorGraph.java
+++ src/util/graph/ColorGraph.java
@@ -1,69 +1,67 @@
 package util.graph;
 
-import util.graph.*;
-import util.intset.*;
-import java.util.*;
-import java.io.*;
+import util.intset.IntSet;
+
+import java.util.Stack;
 
 public class ColorGraph {
-    public  Graph          G;
-    public  int            R;
-    public  int            K;
+    static int NOCOLOR = -1;
+    public Graph G;
+    public int R;
+    public int K;
+    public IntSet enleves;
+    public IntSet deborde;
+    public int[] couleur;
+    public Node[] int2Node;
     private Stack<Integer> pile;
-    public  IntSet         enleves;
-    public  IntSet         deborde;
-    public  int[]          couleur;
-    public  Node[]         int2Node;
-    static  int            NOCOLOR = -1;
 
-    public ColorGraph(Graph G, int K, int[] phi){
-	this.G       = G;
-	this.K       = K;
-	pile         = new Stack<Integer>(); 
-	R            = G.nodeCount();
-	couleur      = new int[R];
-	removed      = new IntSet(R);
-	spill        = new IntSet(R);
-	int2Node     = G.nodeArray();
-	for(int v=0; v < R; v++){
-	    int preColor = phi[v];
-	    if(preColor >= 0 && preColor < K)
-		couleur[v] = phi[v];
-	    else
-		couleur[v] = NOCOLOR;
-	}
+    public ColorGraph(Graph G, int K, int[] phi) {
+        this.G = G;
+        this.K = K;
+        pile = new Stack<Integer>();
+        R = G.nodeCount();
+        couleur = new int[R];
+        enleves = new IntSet(R);
+        deborde = new IntSet(R);
+        int2Node = G.nodeArray();
+        for (int v = 0; v < R; v++) {
+            int preColor = phi[v];
+            if (preColor >= 0 && preColor < K)
+                couleur[v] = phi[v];
+            else
+                couleur[v] = NOCOLOR;
+        }
     }
 
     /*-------------------------------------------------------------------------------------------------------------*/
     /* associe une couleur à tous les sommets se trouvant dans la pile */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void selection()
-    {
+
+    public void selection() {
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* récupère les couleurs des voisins de t */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public IntSet couleursVoisins(int t)
-    {
+
+    public IntSet couleursVoisins(int t) {
+        throw new RuntimeException("Not implemented yet");
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* recherche une couleur absente de colorSet */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int choisisCouleur(IntSet colorSet)
-    {
+
+    public int choisisCouleur(IntSet colorSet) {
+        throw new RuntimeException("Not implemented yet");
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* calcule le nombre de voisins du sommet t */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int nbVoisins(int t)
-    {
+
+    public int nbVoisins(int t) {
+        throw new RuntimeException("Not implemented yet");
     }
 
     /*-------------------------------------------------------------------------------------------------------------*/
@@ -73,36 +71,33 @@ public class ColorGraph {
     /* à la fin du processus, le graphe peut ne pas être vide, il s'agit des temporaires qui ont au moins k voisin */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public int simplification()
-    {
+    public int simplification() {
+        throw new RuntimeException("Not implemented yet");
     }
-    
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void debordement()
-    {
-    }
-
 
     /*-------------------------------------------------------------------------------------------------------------*/
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void coloration()
-    {
-	this.simplification();
-	this.debordement();
-	this.selection();
+    public void debordement() {
+        throw new RuntimeException("Not implemented yet");
     }
 
-    void affiche()
-    {
-	System.out.println("vertex\tcolor");
-	for(int i = 0; i < R; i++){
-	    System.out.println(i + "\t" + couleur[i]);
-	}
+
+    /*-------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------*/
+
+    public void coloration() {
+        this.simplification();
+        this.debordement();
+        this.selection();
     }
-    
-    
+
+    void affiche() {
+        System.out.println("vertex\tcolor");
+        for (int i = 0; i < R; i++) {
+            System.out.println(i + "\t" + couleur[i]);
+        }
+    }
+
 
 }
